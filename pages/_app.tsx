babad6f684077d44209d0da9afb80881a7dd2bf5
Yuki Waka <ywaka@klick.com>
2022-11-03 17:14:28 -0400

    report webvitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+}
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric)
 }
