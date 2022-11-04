d52c7163ccc12c076846f98fea81442ec20919c3
Yuki Waka <ywaka@klick.com>
2022-11-04 14:00:00 -0400

    google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric)
+  return (
+  <>
+  <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-051TGSX3Z7`} /><Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-051TGSX3Z7');`}
+      </Script>
+      <Component {...pageProps} />
+      </>
+  )
 }
